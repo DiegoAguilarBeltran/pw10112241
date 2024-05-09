3aa4cf59b9500cf5182fe0bce4b7101f32668eb7
DiegoAguilarBeltran <116327801+DiegoAguilarBeltran@users.noreply.github.com>
2024-05-09 10:59:12 -0700

    clientesview

--- proyecto/frontend/TIENDA/src/App.vue
+++ proyecto/frontend/TIENDA/src/App.vue
@@ -1,13 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import MenuView from './views/MenuView.vue'
+import ClientesView from './views/ClientesView.vue'
 </script>
 
 <template>
   <header>
-    <h1>
     <MenuView/>
-    </h1>
   </header>
 
   <RouterView />
