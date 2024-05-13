bd5b3833bd26839457e9782cfb11e2f29cac674d
DiegoAguilarBeltran <116327801+DiegoAguilarBeltran@users.noreply.github.com>
2024-05-13 11:03:11 -0700

    lista clientes

--- proyecto/frontend/TIENDA/src/router/index.js
+++ proyecto/frontend/TIENDA/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import MenuView from '../views/MenuView.vue'
+import ClientesView from '../views/ClientesView.vue'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
