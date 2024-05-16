b3d066f6f6c3cd5f0fb76ae8590e800e99e6fefa
DiegoAguilarBeltran <116327801+DiegoAguilarBeltran@users.noreply.github.com>
2024-05-16 10:57:00 -0700

    guardar terminado

--- proyecto/frontend/TIENDA/src/views/ClientesCreateView.vue
+++ proyecto/frontend/TIENDA/src/views/ClientesCreateView.vue
@@ -3,6 +3,9 @@
         <div class="card">
             <div class="card-header">
                 <h4>Agregar Cliente</h4>
+                <div class="alert alert-success" role="alert" v-if="mensaje == 1">
+                    Datos guardados con exito
+                </div>
             </div>
             <div class="card-body">
                 <div class="mb-3">
@@ -11,34 +14,34 @@
                 </div>
                 <div class="mb-3">
                     Nombre
-                    <input type="number" class="form-control" v-model="model.cliente.nombre">
+                    <input type="text" class="form-control" v-model="model.cliente.nombre">
                 </div>
                 <div class="mb-3">
                     Apellido
-                    <input type="number" class="form-control" v-model="model.cliente.apellido">
+                    <input type="text" class="form-control" v-model="model.cliente.apellido">
                 </div>
                 <div class="mb-3">
                     Direccion
-                    <input type="number" class="form-control" v-model="model.cliente.direccion">
+                    <input type="text" class="form-control" v-model="model.cliente.direccion">
                 </div>
                 <div class="mb-3">
                     Telefono
-                    <input type="number" class="form-control" v-model="model.cliente.telefono">
+                    <input type="text" class="form-control" v-model="model.cliente.telefono">
                 </div>
                 <div class="mb-3">
                     RFC
-                    <input type="number" class="form-control" v-model="model.cliente.rfc">
+                    <input type="text" class="form-control" v-model="model.cliente.rfc">
                 </div>
                 <div class="mb-3">
                     CURP
-                    <input type="number" class="form-control" v-model="model.cliente.curp">
+                    <input type="text" class="form-control" v-model="model.cliente.curp">
                 </div>
                 <div class="mb-3">
                     Codigo Postal
-                    <input type="number" class="form-control" v-model="model.cliente.cp">
+                    <input type="text" class="form-control" v-model="model.cliente.cp">
                 </div>
                 <div class="mb-3">
-                    <button class="btn btn-primary">Guardar</button>
+                    <button class="btn btn-primary" @click="guardarCliente()">Guardar</button>
                 </div>
             </div>    
         </div>
@@ -46,10 +49,12 @@
 </template>
 
 <script>
+import axios from 'axios';
     export default {
         name: "ClientesCreate",
         data(){
             return{
+                mensaje: 0,
                 model: {
                     cliente:{
                         id: '',
@@ -62,6 +67,26 @@
                         cp: ''
                     }
                 }
+            }
+        },
+        methods:{
+            guardarCliente(){
+                console.log("Mando");
+                axios.post('http://localhost:3000/api/clientes', this.model.cliente).then(res=>{
+                    if(res.data.affectedRows == 1){ //si insertamos un registro
+                        this.model.cliente = {
+                        id: '',
+                        nombre: '',
+                        apellido: '',
+                        direccion: '',
+                        telefono: '',
+                        rfc: '',
+                        curp: '',
+                        cp: ''
+                    }
+                        this.mensaje = 1;
+                    }
+                })
             }
         }
     }
